220dfad915cee1905ba610e018bb9090111d9a25
Xu Zheng <slowpolitics@outlook.com>
2024-10-14 17:58:55 +0800

    Initial commit

---
+++ src/main/kotlin/tech/xuzheng/forwork/memoserverkt/MemoServerKtApplication.kt
@@ -0,0 +1,11 @@
+package tech.xuzheng.forwork.memoserverkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MemoServerKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<MemoServerKtApplication>(*args)
+}
---
+++ src/test/kotlin/tech/xuzheng/forwork/memoserverkt/MemoServerKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.xuzheng.forwork.memoserverkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MemoServerKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
